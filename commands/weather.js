b99192fcb97210f6a9ed696d75d350282c0e0374
ᴍʀ ᴍᴏsᴇs <marinyamestudios@gmail.com>
2025-06-22 00:02:48 +0200

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -5,19 +5,7 @@ module.exports = async function (sock, chatId, city) {
         const apiKey = '4902c0f2550f58298ad4146a92b65e10';  // Replace with your OpenWeather API Key
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
         const weather = response.data;
-        const weatherText = `🌍 *Weather Information for ${data.name}, ${data.sys.country}* 🌍
-🌡️ *Temperature*: ${data.main.temp}°C
-🌡️ *Feels Like*: ${data.main.feels_like}°C
-🌡️ *Min Temp*: ${data.main.temp_min}°C
-🌡️ *Max Temp*: ${data.main.temp_max}°C
-💧 *Humidity*: ${data.main.humidity}%
-☁️ *Weather*: ${data.weather[0].main}
-🌫️ *Description*: ${data.weather[0].description}
-💨 *Wind Speed*: ${data.wind.speed} m/s
-🔽 *Pressure*: ${data.main.pressure} hPa
-
-*_powered by mr moses clr_*
-`;
+        const weatherText = `Weather in ${weather.name}: ${weather.weather[0].description}. Temperature: ${weather.main.temp}°C.`;
         await sock.sendMessage(chatId, { text: weatherText });
     } catch (error) {
         console.error('Error fetching weather:', error);
