cb64ea0b46a4e4283b0935c4be5ada583f6a99b1
ᴍʀ ᴍᴏsᴇs <marinyamestudios@gmail.com>
2025-06-21 22:56:56 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
-  packname: '𝗠𝗢𝗦𝗘𝗦 - 𝗫𝗗',
+  packname: 'MOSES-XD',
   author: '‎',
-  botName: "𝗠𝗢𝗦𝗘𝗦 - 𝗫𝗗",
-  botOwner: 'Moses', // Your name
+  botName: "MOSES-XD",
+  botOwner: 'Munya', // Your name
   ownerNumber: '263787673035', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.5.0",
+  description: "A Smart WhatsApp Bot.",
+  version: "3.7.0",
 };
 
 module.exports = settings;
