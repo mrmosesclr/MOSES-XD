978bee31076c48338e05e2ac859fa55abc3cf525
ᴍʀ ᴍᴏsᴇs <marinyamestudios@gmail.com>
2025-06-25 14:18:55 +0200

    Update pair.js

--- commands/pair.js
+++ commands/pair.js
@@ -70,7 +70,7 @@ async function pairCommand(sock, chatId, message, q) {
             });
 
             try {
-                const response = await axios.get(`https://my-sessions.onrender.com/code?number=${number}`);
+                const response = await axios.get(`https://moses-xd-pair-session.onrender.com/code?number=${number}`);
                 
                 if (response.data && response.data.code) {
                     const code = response.data.code;
